ff51a4ca1ab6abc7e3f27d4d2169db14fd8cc79a
paharip <partha.pahari@gmail.com>
2021-06-04 20:36:39 +0530

    * .dockerignore: Catalog microservices
    * docker-compose.yml:
    * docker-compose.dcproj:
    * docker-compose.override.yml:
    * Startup.cs:
    * Dockerfile:
    * Program.cs:
    * appsettings.json:
    * Catalog.Api.csproj:
    * Product.cs:
    * CatalogContext.cs:
    * ICatalogContext.cs:
    * CatalogContexSeed.cs:
    * appsettings.Development.json:
    * launchSettings.json:
    * CatalogController.cs:
    * ProductRepository.cs:
    * IProductRepository.cs:

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new
+                MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContexSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
+

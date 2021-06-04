abd6dd6f738f8a1dfa1c1b0608f43fe5365362b8
paharip <partha.pahari@gmail.com>
2021-06-04 23:02:35 +0530

    * Program.cs: * Startup.cs: * appsettings.json: * Basket.Api.csproj: * ShoppingCart.cs: * BasketCheckout.cs: * ShoppingCartItem.cs: * appsettings.Development.json: * launchSettings.json: * BasketController.cs: * BasketRepository.cs: * IBasketRepository.cs:
    * aspnetrun-microservices.sln: Adding BasketApi Intial commit

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice +=  item.Price * item.Quantity;
+                }
+
+                return totalprice;
+            }
+        }
+
+
+    }
+}
